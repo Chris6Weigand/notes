37e3fe1fe10762d12b53c98759734d9a29db9437
Chris6Weigand <145224777+Chris6Weigand@users.noreply.github.com>
2024-11-14 23:56:58 -0500

    Update lab11.sql

--- lab11.sql
+++ lab11.sql
@@ -1,1 +1,41 @@
+--Practice
 
+SELECT * FROM courses;
+
+SELECT * FROM assignments
+LIMIT 10; 
+
+SELECT count(*) FROM courses;
+
+SELECT min(due_date) FROM assignments;
+
+SELECT *
+FROM  courses
+WHERE course_name LIKE 'Intro%';
+
+SELECT *
+FROM  assignments
+WHERE status != 'Completed'
+ORDER BY due_date;
+
+-- Exercise Questions
+
+SELECT * FROM assignments
+WHERE course_id LIKE 'COMP1234'
+ORDER BY due_date;
+
+SELECT min(due_date) FROM assignments
+
+SELECT max(due_date) FROM assignments
+
+SELECT course_id, title
+FROM  assignments
+WHERE due_date = '2024-10-08';
+
+SELECT course_id, due_date
+FROM  assignments
+WHERE due_date LIKE '2024-10%';
+
+SELECT min(due_date)
+FROM assignments
+WHERE status='Completed';
